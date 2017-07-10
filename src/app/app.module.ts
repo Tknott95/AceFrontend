3f725da078cea8c987db3f8e3aa2a69677dd6259
tknott95 <tknot95@hotmail.com>
2017-07-10 04:40:16 -0600

    Formed - TK

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,41 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule, NoopAnimationsModule } from '@angular/platform-browser/animations';
+
+import { 
+  MdToolbarModule,
+  MdSidenavModule,
+  MdListModule,
+  MdProgressSpinnerModule,
+  MdCardModule,
+  MdInputModule,
+  MdCheckboxModule,
+  MdGridListModule
+ } from '@angular/material';
+
+
+/* Gesture Support (W/ M Design) */
+import 'hammerjs';
+import { NavComponent } from './components/nav/nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    NoopAnimationsModule,
+    MdToolbarModule,
+  MdSidenavModule,
+  MdListModule,
+  MdProgressSpinnerModule,
+  MdCardModule,
+  MdInputModule,
+  MdCheckboxModule,
+  MdGridListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
