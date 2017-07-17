a31f1d008613620900b38290167cc2f165e63d01
Tknott95 <tknott95@hotmail.com>
2017-07-17 03:21:29 -0600

    Added new about image - TK

--- src/app/components/pages/about/about.component.ts
+++ src/app/components/pages/about/about.component.ts
@@ -10,7 +10,7 @@ export class AboutComponent implements OnInit {
     imgTexWBaby: string = 'https://pbs.twimg.com/profile_images/797636250869997570/7_MI-vSf.jpg';
   imgDugouting: string = 'https://v.cdn.vine.co/r/avatars/B68F437E-F58B-4598-88A6-E9A9BDBF4F50-748-000000A25992102776f5aea9d3.jpg?versionId=mIetbj5d9Pb.MagwzYn7WzAXBwqCftVi';
 
-  mainCardImg: string = this.imgDugouting;
+  mainCardImg: string = '../../assets/tk.jpg';
 
   constructor() { }
 
